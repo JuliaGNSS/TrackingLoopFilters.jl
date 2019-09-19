1d516f92987d8700d3c9073e3f039ad05c2b26fc
unknown <erik.deinzer@rwth-aachen.de>
2019-09-19 20:48:28 +0200

    Module wrapped

--- src/TrackingLoopFilters.jl
+++ src/TrackingLoopFilters.jl
@@ -1,22 +1,26 @@
+module TrackingLoopFilters
 
-    using StaticArrays, LinearAlgebra
+    using StaticArrays, Unitful
     import Unitful: MHz, kHz, Hz, s, ms, upreferred
-  
+    
+    
 
-include("FilterStructs.jl")
+    include("FilterStructs.jl")
 
 
-function loop_filter(state::T, δθ, Δt, bandwidth) where T<:AbstractLoopFilter
-    ω₀ = Float64(bandwidth / Hz) * 4.0
-    Δt_sec = Float64(upreferred(Δt/s))
-    propagate(state, δθ, Δt_sec, bandwidth)
-    out = filtered_output(state, δθ, Δt_sec, bandwidth) * Hz
+    function loop_filter(state::T, δθ, Δt, bandwidth) where T<:AbstractLoopFilter
+        ω₀ = Float64(bandwidth / Hz) * 4.0
+        Δt_sec = Float64(upreferred(Δt/s))
+        propagate(state, δθ, Δt_sec, bandwidth)
+        out = filtered_output(state, δθ, Δt_sec, bandwidth) * Hz
+    end
+
+
+    include("FirstOrderLF.jl")
+    include("SecondOrderLF.jl")
+    include("ThirdOrderLF.jl") 
+    
 end
-
-
-include("FirstOrderLF.jl")
-include("SecondOrderLF.jl")
-include("ThirdOrderLF.jl")   
 
 
 
